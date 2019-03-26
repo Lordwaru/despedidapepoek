c693092ecdbaaf2cba29113a888644d89b797e9b
Daniel Ortiz <LalabumEducacion@Mercedez.local>
2019-03-26 11:37:19 -0700

    el admin se la come

--- src/components/admin-status.vue
+++ src/components/admin-status.vue
@@ -1,5 +1,26 @@
 <template>
     <div>
-        
+        <v-layout class="primary" align-center justify-center fill-height>
+            <v-flex xs12>
+                <h1 pa0 ma0 class="admin-header" >El admin se la come</h1>
+            </v-flex>
+        </v-layout>
     </div>
 </template>
+
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Fjalla+One');
+
+body{
+    margin: 0;
+}
+
+.admin-header {
+    font-family: 'Fjalla One', sans-serif;
+    font-size: 10vw;
+    text-align: center;
+    color:floralwhite;
+}
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
-import './plugins/vuetify'
+import Vuetify from 'vuetify'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+Vue.use(Vuetify,{
+  theme: {
+    primary: '#e91e63',
+    secondary: '#607d8b',
+    accent: '#4caf50',
+    error: '#ff9800',
+    warning: '#ffc107',
+    info: '#00bcd4',
+    success: '#3f51b5'
+    }
+  }
+)
 
 new Vue({
   render: h => h(App),
